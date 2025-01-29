a34d8e1289a87e84af2aec2c8f56a5e77b90f7b7
i-nirbhay10 <nitbhayverma10@gmail,com>
2025-01-29 15:59:23 +0530

    new update

--- src/components/Display.js
+++ src/components/Display.js
@@ -13,6 +13,11 @@ const Display = () => {
 
   const fetchApi = async () => {
     try {
+      if (!search) {
+        alert("please enter the data");
+        return;
+      }
+
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${process.env.REACT_APP_KEY}`;
       const response = await fetch(url);
       const res = await response.json();
@@ -53,15 +58,27 @@ const Display = () => {
   return (
     <>
       <Nav value={search} hummi={data.humidity} />
-      <div className="all d-flex justify-content-evenly ">
-        <div className="lpart d-none d-sm-block rounded bg-dark bg-opacity-80 text-light mx-2 my-2 ">
+      <div className="d-flex justify-content-evenly ">
+        <div className="lpart d-none d-sm-block rounded text-dark mx-2 my-2 ">
           <div className="mx-4 my-4 py-3 ">
             <h2 className="text-capitalize">Abou the web app</h2>
             <h5>Devloper :- Nirbhay verma</h5>
             <p className="text-justify  py-2 px-2 bg-info bg-opacity-10 rounded ms-2 ">
-              This web app is Developed by Nirbhay he is a full stack dewloper .
-              the web app use for real time weather information if the user
-              search any city then tha app give the result for the same city
+              I’m a passionate MERN stack developer with a strong focus on
+              crafting seamless web and mobile applications that make a real
+              impact. Specializing in React.js and React Native, I deliver
+              high-quality, cross-platform solutions that provide consistent
+              user experiences across devices. Leveraging Node.js, Express.js,
+              and MongoDB on the back end, I build scalable and feature-rich
+              applications tailored to client needs. In my work as a full-stack
+              developer, I’ve led the development of a range of projects, from
+              full-featured web applications like Wiregram (an
+              Instagram-inspired app with secure Google OAuth and JWT
+              integration) to mobile portfolio apps showcasing skills and
+              projects with React Native’s responsive, performance-focused
+              architecture. My experience extends to real-time applications for
+              news and weather, API integration, and deployment across Netlify
+              and Render, ensuring optimal functionality and performance.
             </p>
           </div>
         </div>
@@ -91,30 +108,30 @@ const Display = () => {
             {code === 200 ? (
               <>
                 <div>
-                  <h2 className="text-center text-capitalize text-warning mt-3">
+                  <h2 className="text-center text-capitalize text-dark mt-3">
                     <i
-                      className="bi bi-geo-alt-fill text-light"
+                      className="bi bi-geo-alt-fill text-dark"
                       style={{ fontSize: "3rem" }}
                     ></i>
                     {search}
                   </h2>
                 </div>
-                <div className="d-flex mt-4 justify-content-evenly">
+                <div className="d-flex mt-4 justify-content-evenly text-dark">
                   <h3>
                     <i class="bi bi-thermometer-half text-danger"></i>
-                    <heading> {data.temp} &deg;C </heading>
+                    <heading className="text-dark">{data.temp} &deg;C</heading>
                   </h3>
                   <h3>{wtype}</h3>
                 </div>
                 <Wicon para={wtype} />
-                <div className="d-flex my-3 justify-content-evenly">
+                <div className="d-flex my-3 justify-content-evenly text-dark">
                   <h4>
                     Max {data.temp_max} &deg;C | Min{data.temp_min} &deg;C
                   </h4>
                 </div>
               </>
             ) : code === 404 ? (
-              <h4 className="text-center text-capitalize text-info mt-3">
+              <h4 className="text-center text-capitalize text-dark mt-3">
                 {search} is not a city
               </h4>
             ) : (
